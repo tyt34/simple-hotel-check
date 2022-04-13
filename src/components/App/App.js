32d617e4dd814322c01b76611fbf73b1e9859ff5
R N <novoto4in@mail.ru>
2022-04-13 18:55:58 +0300

    deploy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Main from '../Main/Main'
 import Auth from '../Auth/Auth'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { HashRouter, Routes, Route, Switch} from 'react-router-dom'
 ///simple-hotel-check
 function App() {
   return (
-    <BrowserRouter basename='/simple-hotel-check'>
+    <HashRouter basename='/simple-hotel-check'>
         <Routes>
           <Route path="/" element={
             <>
@@ -18,7 +18,7 @@ function App() {
             </>
           } />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
