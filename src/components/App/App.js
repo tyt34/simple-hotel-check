93976a75c0ee320b242081afd6640091a38f1cc3
R N <novoto4in@mail.ru>
2022-04-13 18:49:56 +0300

    deploy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import Main from '../Main/Main'
 import Auth from '../Auth/Auth'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-
+///simple-hotel-check
 function App() {
   return (
     <BrowserRouter basename='/simple-hotel-check'>
         <Routes>
-          <Route path="/main" element={
+          <Route path="/" element={
             <>
               <Main/>
             </>
           } />
-        </Routes>
-        <Routes>
           <Route path="/auth" element={
             <>
               <Auth/>
