424dd9a0dafd041d766bb4b3857e2ed58586bd4c
R N <novoto4in@mail.ru>
2022-04-13 18:14:53 +0300

    deploy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLICK_URL}>
         <Routes>
           <Route path="/" element={
             <>
